784a12ae846023da8991d169116a7fb85747bb27
Salma Gamal <salmagamal1515@hotmail.com>
2022-05-17 12:34:41 +0200

    -

--- js/myScript.js
+++ js/myScript.js
@@ -1,3 +1,7 @@
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show2");
+};
 $(document).ready(function(){
 $('#menu-button').click(function(e){
     e.stopPropagation();
@@ -12,4 +16,36 @@ $('#menu-button').click(function(e){
     $('body,html').click(function(e){
            $('#mob-menu').removeClass('show-menu');
     });
+    
+     $('.close-pop-up').click(function(e){
+		$('#myPopup').removeClass('show2');   
+    }); 
+    
+    
+    $('.edit-email-icon').click(function(e){
+		$('.edit-email-div').addClass('show2');   
+    }); 
+    $('.close-edit-pop').click(function(e){
+		$('.edit-email-div').removeClass('show2');   
+    });
+    
+    
+    $('.edit-psw-btn').click(function(e){
+		$('.edit-psw-div').addClass('show2');   
+    }); 
+    $('.close-edit-pop').click(function(e){
+		$('.edit-psw-div').removeClass('show2');   
+    });
+
+    
+    $('.edit-info-icon').click(function(e){
+		$('.edit-info-div').addClass('show2');   
+    }); 
+    $('.close-edit-pop').click(function(e){
+		$('.edit-info-div').removeClass('show2');   
+    });
+
+    
+    
+
 });
